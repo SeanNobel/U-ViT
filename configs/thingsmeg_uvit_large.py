ff957dca41cfd9425600d87608c103289dad2880
SeanNobel <sensho19950503@gmail.com>
2024-02-09 17:49:01 +0900

    batch size 256 -> 128

--- configs/thingsmeg_uvit_large.py
+++ configs/thingsmeg_uvit_large.py
@@ -20,7 +20,7 @@ def get_config():
     config.train = d(
         name="default",
         n_steps=1000000,
-        batch_size=256,
+        batch_size=128,
         log_interval=10,
         eval_interval=5000,
         save_interval=50000,
